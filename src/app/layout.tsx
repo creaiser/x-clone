d20c9468d23331383b6e0b7b3bff91a8071a61ed
KUMIR2006 <kurbanovmirzobek@gmail.com>
2025-08-22 00:42:58 +0300

    feat: Left Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,11 +9,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <div className="flex justify-between items-center h-screen">
-          <div className=""><LeftBar /></div>
-          <div className="">{children}</div>
-          <div className=""><RightBar /></div>
+      <body >
+        <div className="flex justify-between mx-auto max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl">
+          <div className="px-2 xsm:px-4 xxl:px-8 h-screen">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray h-screen">{children}</div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 h-screen">
+            <RightBar />
+          </div>
         </div>
       </body>
     </html>
