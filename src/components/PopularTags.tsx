12be6a7a356a520cd713b50f1232015c5ac9229f
KUMIR2006 <kurbanovmirzobek@gmail.com>
2026-02-01 23:42:00 +0300

    feat: responsive RightBar

---
+++ src/components/PopularTags.tsx
@@ -0,0 +1,59 @@
+import Image from './Image'
+import Link from 'next/link'
+
+const PopularTags = () => {
+    return (
+        <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+          <h1 className="text-xl fond-bold text-textGrayLight">{"What's"} Happening?</h1>
+
+          {/*TREND EVENT*/}
+          <div className="flex gap-4 ">
+            <div className="relative w-20 h-20 rounded-xl overflow-hidden">
+              <Image path="general/event.png" alt="event" w={120} h={120} tr={true}/>
+              </div>
+            <div className="flex-1">
+              <h2 className='font-bold text-textGrayLight'>Nadal vs Federer Grand Slam</h2>
+              <span className='text-sm text-textGray '>Last Night</span>
+            </div>
+          </div>
+
+
+          {/*TOPICS*/}
+          <div className="">
+            <div className="flex items-center justify-between">
+              <span className="text-textGray text-sm">Technology * Trending</span>
+              <Image path="icons/infoMore.svg" alt="info" w={16} h={16}/>
+            </div>
+            <h2 className="text-textGrayLight font-bold">OpenAI</h2>
+            <span className='text-sm text-textGray '>20K posts</span>
+          </div>
+          <div className="">
+            <div className="flex items-center justify-between">
+              <span className="text-textGray text-sm">Technology * Trending</span>
+              <Image path="icons/infoMore.svg" alt="info" w={16} h={16}/>
+            </div>
+            <h2 className="text-textGrayLight font-bold">OpenAI</h2>
+            <span className='text-sm text-textGray '>20K posts</span>
+          </div>
+          <div className="">
+            <div className="flex items-center justify-between">
+              <span className="text-textGray text-sm">Technology * Trending</span>
+              <Image path="icons/infoMore.svg" alt="info" w={16} h={16}/>
+            </div>
+            <h2 className="text-textGrayLight font-bold">OpenAI</h2>
+            <span className='text-sm text-textGray '>20K posts</span>
+          </div>
+          <div className="">
+            <div className="flex items-center justify-between">
+              <span className="text-textGray text-sm">Technology * Trending</span>
+              <Image path="icons/infoMore.svg" alt="info" w={16} h={16}/>
+            </div>
+            <h2 className="text-textGrayLight font-bold">OpenAI</h2>
+            <span className='text-sm text-textGray '>20K posts</span>
+          </div>
+          <Link href="/" className="text-iconBlue">Show more</Link>
+        </div>
+    )
+}
+
+export default PopularTags;
---
+++ src/components/Recommendations.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import Image from './Image'
+
+const Recommendations = () => (
+    <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4">
+        {/* USER CARD */}
+        <div className="flex items-center justify-between">
+            {/* Image and user info */}
+            <div className="flex items-center gap-2">
+                <div className="relative rounded-full overflow-hidden w-10 h-10">
+                    <Image path="general/avatar.png" alt="creaiser" w={100} h={100} tr={true} />
+                </div>
+                <div className="">
+                    <h1 className="text-md font-bold">Creaiser</h1>
+                    <span className="text-textGray text-sm">@creaiser</span>
+                </div>
+            </div>
+             {/* BUTTON */}
+            <Link href="/" className="py-1 px-4 rounded-full font-semibold bg-white text-black">Follow</Link>
+        </div>
+        <div className="flex items-center justify-between">
+            {/* Image and user info */}
+            <div className="flex items-center gap-2">
+                <div className="relative rounded-full overflow-hidden w-10 h-10">
+                    <Image path="general/avatar.png" alt="creaiser" w={100} h={100} tr={true} />
+                </div>
+                <div className="">
+                    <h1 className="text-md font-bold">Creaiser</h1>
+                    <span className="text-textGray text-sm">@creaiser</span>
+                </div>
+            </div>
+             {/* BUTTON */}
+            <Link href="/" className="py-1 px-4 rounded-full font-semibold bg-white text-black">Follow</Link>
+        </div>
+        <div className="flex items-center justify-between">
+            {/* Image and user info */}
+            <div className="flex items-center gap-2">
+                <div className="relative rounded-full overflow-hidden w-10 h-10">
+                    <Image path="general/avatar.png" alt="creaiser" w={100} h={100} tr={true} />
+                </div>
+                <div className="">
+                    <h1 className="text-md font-bold">Creaiser</h1>
+                    <span className="text-textGray text-sm">@creaiser</span>
+                </div>
+            </div>
+             {/* BUTTON */}
+            <Link href="/" className="py-1 px-4 rounded-full font-semibold bg-white text-black">Follow</Link>
+        </div>
+        <Link href="/" className="text-iconBlue">Show more</Link>
+    </div>
+)
+
+export default Recommendations;
