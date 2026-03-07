245f316833dbb8d856d385f86b88724f62e2780b
KUMIR2006 <kurbanovmirzobek@gmail.com>
2026-03-07 22:19:10 +0300

    feat: set up Clerk Auth

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/generated/client";
+import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 declare global {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,18 @@
-import RightBar from "@/components/RightBar";
 import "./globals.css";
-import LeftBar from "@/components/LeftBar";
-import { ImageKitProvider } from '@imagekit/next';
+import { ClerkProvider } from "@clerk/nextjs";
 
-export default function RootLayout({
+export default function ApplLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body>
-        <ImageKitProvider urlEndpoint={process.env.PUBLIC_IMAGEKIT_URL_ENDPOINT!}>
-          {/* MODAL POST CREATING  */}
-          <div>
-            {modal}
-          </div>
-          
-
-          <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex relative">
-            
-            {/* Left Bar - Fixed */}
-            <div className="w-[69px] sm:w-[89px] lg:w-[69px] xl:w-[89px]  xxl:w-[277px] shrink-0">
-              <LeftBar />
-            </div>
-            
-            {/* Main Content */}
-            <div className="flex-1 lg:max-w-[600px]  border-borderGray">
-              {children}
-            </div>
-            
-            {/* Right Bar - Fixed */}
-            <div className="hidden lg:block  w-[290px] xl:w-[350px] shrink-0">
-              <RightBar />
-            </div>
-          </div>
-        </ImageKitProvider>
+        {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
