b622b0542dd6970dc0cdc5f0da6a2b1bccc8e59a
KUMIR2006 <kurbanovmirzobek@gmail.com>
2026-03-10 15:18:10 +0300

    feat: add data to Recommendations component

--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -1,89 +1,67 @@
 import Link from 'next/link'
 import Image from './Image'
+import { auth } from '@clerk/nextjs/server'
+import { prisma } from '@/prisma'
 
-const Recommendations = () => (
-  <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4 ">
-    <h1 className="text-xl fond-bold text-textGrayLight">Who to follow</h1>
-    {/* USER CARD */}
-    <div className="flex items-center justify-between">
-      {/* Image and user info */}
-      <div className="flex items-center gap-2">
-        <div className="relative rounded-full overflow-hidden w-10 h-10">
-          <Image
-            path="general/avatar.png"
-            alt="creaiser"
-            w={100}
-            h={100}
-            tr={true}
-          />
+const Recommendations = async () => {
+  const { userId } = await auth()
+  if (!userId) return
+
+  const followingIds = await prisma.follow.findMany({
+    where: { followerId: userId },
+    select: { followingId: true },
+  })
+  const followedUserIds = followingIds.map((f) => f.followingId)
+
+  const friendRecommendations = await prisma.user.findMany({
+    where: {
+      id: { not: userId, notIn: followedUserIds },
+      followings: { some: { followerId: { in: followedUserIds } } },
+    },
+    take: 3,
+    select: { id: true, displayName: true, username: true, img: true },
+  })
+
+  return (
+    <div className="p-4 rounded-2xl border-[1px] border-borderGray flex flex-col gap-4 ">
+      <h1 className="text-xl fond-bold text-textGrayLight">Who to follow</h1>
+
+      {/* USER CARD */}
+      {friendRecommendations.map((person) => (
+        <div className="flex items-center justify-between" key={person.id}>
+          {/* Image and user info */}
+          <div className="flex items-center gap-2">
+            <div className="relative rounded-full overflow-hidden w-10 h-10">
+              <Image
+                path={person.img || 'general/noAvotar.png'}
+                alt={person.username}
+                w={100}
+                h={100}
+                tr={true}
+              />
+            </div>
+            <div className="">
+              <h1 className="text-md font-bold">
+                {person.displayName || person.username}
+              </h1>
+              <span className="text-textGray text-sm">@{person.username}</span>
+            </div>
+          </div>
+          {/* BUTTON */}
+          <Link
+            href="/"
+            className="py-1 px-4 rounded-full font-semibold bg-white text-black"
+          >
+            Follow
+          </Link>
         </div>
-        <div className="">
-          <h1 className="text-md font-bold">Creaiser</h1>
-          <span className="text-textGray text-sm">@creaiser</span>
-        </div>
-      </div>
-      {/* BUTTON */}
-      <Link
-        href="/"
-        className="py-1 px-4 rounded-full font-semibold bg-white text-black"
-      >
-        Follow
+      ))}
+
+      <Link href="/" className="text-iconBlue">
+        Show more
       </Link>
     </div>
-    <div className="flex items-center justify-between">
-      {/* Image and user info */}
-      <div className="flex items-center gap-2">
-        <div className="relative rounded-full overflow-hidden w-10 h-10">
-          <Image
-            path="general/avatar.png"
-            alt="creaiser"
-            w={100}
-            h={100}
-            tr={true}
-          />
-        </div>
-        <div className="">
-          <h1 className="text-md font-bold">Creaiser</h1>
-          <span className="text-textGray text-sm">@creaiser</span>
-        </div>
-      </div>
-      {/* BUTTON */}
-      <Link
-        href="/"
-        className="py-1 px-4 rounded-full font-semibold bg-white text-black"
-      >
-        Follow
-      </Link>
-    </div>
-    <div className="flex items-center justify-between">
-      {/* Image and user info */}
-      <div className="flex items-center gap-2">
-        <div className="relative rounded-full overflow-hidden w-10 h-10">
-          <Image
-            path="general/avatar.png"
-            alt="creaiser"
-            w={100}
-            h={100}
-            tr={true}
-          />
-        </div>
-        <div className="">
-          <h1 className="text-md font-bold">Creaiser</h1>
-          <span className="text-textGray text-sm">@creaiser</span>
-        </div>
-      </div>
-      {/* BUTTON */}
-      <Link
-        href="/"
-        className="py-1 px-4 rounded-full font-semibold bg-white text-black"
-      >
-        Follow
-      </Link>
-    </div>
-    <Link href="/" className="text-iconBlue">
-      Show more
-    </Link>
-  </div>
-)
+  )
+}
 
 export default Recommendations
